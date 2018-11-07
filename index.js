0708fa3174a3561cf90f4967646478bd3cc7ac55
Michael Granados <michaelgranados@gmail.com>
2018-11-07 10:15:16 -0200

    fix eslint problems
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- index.js
+++ index.js
@@ -7,111 +7,107 @@ const uuidv4 = require('uuid/v4');
 
 const server = http.createServer();
 
+const winstonLogger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+
 class Controller {
-  routes(req) {
+  static routes(req) {
     const routes = [
-      ['GET', /^\/$/, this.home],
-      ['GET', /^\/api\/?$/, this.api],
-      ['GET', /^\/api\/boxes\/?$/, this.apiBoxes],
-      ['GET', /^\/api\/boxes\/(?<boxId>\d+)\/?$/, this.apiBox],
+      ['GET', /^\/$/, Controller.home],
+      ['GET', /^\/api\/?$/, Controller.api],
+      ['GET', /^\/api\/boxes\/?$/, Controller.apiBoxes],
+      ['GET', /^\/api\/boxes\/(?<boxId>\d+)\/?$/, Controller.apiBox],
     ];
 
-    const parsedUrl = url.parse(req.url, true)
-    const { pathname  } = parsedUrl;
+    const parsedUrl = url.parse(req.url, true);
+    const { pathname } = parsedUrl;
     const method = req.method.toUpperCase();
 
     return routes
       .filter(route => method === route[0].toUpperCase())
-      .find(route => {
-	const matches = pathname.match(route[1]);
-	if (matches) {
-	  route.params = matches.groups;
-	  route.query = parsedUrl.query;
-	  route.pathname = pathname;
-	  return matches;
-	}
+      .find((route) => {
+        const matches = pathname.match(route[1]);
+        if (matches) {
+          Object.assign(route, { params: matches.groups, query: parsedUrl.query, pathname });
+        }
+        return matches;
       });
   }
 
-  createLogger({ req, res }) {
-    const logger = winston.createLogger({
-      level: 'info',
-      format: winston.format.json(),
-      transports: [
-	new winston.transports.Console(),
-      ]
-    });
-
+  static createLogger({ req }) {
     const loggerInstance = {};
 
     Object.keys(winston.config.npm.levels).forEach((level) => {
       loggerInstance[level] = (...args) => {
-	const info = {};
-	info.thread = req.thread;
-	info.method = req.method;
-	info.level = level;
-	info.url = req.url;
-	info.date = new Date().toJSON();
+        const info = {};
+        info.thread = req.thread;
+        info.method = req.method;
+        info.level = level;
+        info.url = req.url;
+        info.date = new Date().toJSON();
 
-	if (args.length === 1 && typeof args[0] === "object") {
-	  Object.assign(info, args[0]);
-	  return logger[level].call(logger, info);
-	}
+        if (args.length === 1 && typeof args[0] === 'object') {
+          Object.assign(info, args[0]);
+          return winstonLogger[level].call(winstonLogger, info);
+        }
 
-	args = [ util.format(...args), info ];
-	return logger[level].apply(logger, args);
-      }
+        return winstonLogger[level](...[util.format(...args), info]);
+      };
     });
 
     return loggerInstance;
   }
 
-  logResponse({ logger, req, res }) {
-    logger.info({ message: 'response', responseStatus: res.statusCode })
+  static logResponse({ logger, res }) {
+    logger.info({ message: 'response', responseStatus: res.statusCode });
   }
 
-  async home({ req, res }) {
+  static async home({ res }) {
     const page = await fs.readFileSync(`${__dirname}/templates/index.html`);
     res.writeHead(200);
     res.write(page.toString());
     res.end();
   }
 
-  async api({ req, res }) {
+  static async api({ res }) {
     res.writeHead(200, { 'Content-Type': 'application/hal+json' });
     res.write(JSON.stringify({
       _links: {
-	self: { href: '/api' },
-	boxes: { href: '/api/boxes'  },
+        self: { href: '/api' },
+        boxes: { href: '/api/boxes' },
       },
     }));
     res.end();
   }
 
-  async apiBoxes({ req, res }) {
+  static async apiBoxes({ res }) {
     res.writeHead(200, { 'Content-Type': 'application/hal+json' });
     res.write(JSON.stringify({
       _links: {
-	api: { href: '/api' },
-	self: { href: '/api/boxes'  },
-	box: { href: '/api/boxes/:boxId', templated: true  },
+        api: { href: '/api' },
+        self: { href: '/api/boxes' },
+        box: { href: '/api/boxes/:boxId', templated: true },
       },
       _embedded: {
-	boxes: [], // TODO: populate
+        boxes: [], // TODO: populate
       },
     }));
     res.end();
   }
 
-  async apiBox({ req, res }) {
-    const info = url.parse(req.url, true);
-
+  static async apiBox({ req, res }) {
     res.writeHead(200, { 'Content-Type': 'application/hal+json' });
     res.write(JSON.stringify({
       _links: {
-	api: { href: '/api' },
-	boxes: { href: '/api/boxes'  },
-	self: { href: `/api/boxes/${req.params.boxId}` },
+        api: { href: '/api' },
+        boxes: { href: '/api/boxes' },
+        self: { href: `/api/boxes/${req.params.boxId}` },
       },
       // TODO: populate attributes
     }));
@@ -119,40 +115,37 @@ class Controller {
   }
 }
 
-const controller = new Controller();
-
 server.on('request', async (req, res) => {
   req.thread = uuidv4();
-  const logger = controller.createLogger({ req, res });
+  const logger = Controller.createLogger({ req, res });
   logger.info('received request');
 
-  const route = controller.routes(req)
+  const route = Controller.routes(req);
 
   if (route) {
     req.params = route.params;
     req.query = route.query;
     req.pathname = route.pathname;
 
-    context = { req, res, logger };
+    const context = { req, res, logger };
 
-    let promise = Promise.resolve();
+    const promise = Promise.resolve();
 
-    route.push(controller.logResponse);
+    route.push(Controller.logResponse);
 
     route.slice(2).forEach((action) => {
       promise.then(async () => {
-	action(context);
-	return;
-      })
+        action(context);
+      });
     });
     return;
   }
 
   res.statusCode = 404;
-  res.end("");
-  controller.logResponse(req, res);
+  res.end('');
+  Controller.logResponse(req, res);
 });
 
 server.listen(8000, () => {
-  console.info('Listening 0.0.0.0:8000')
+  winstonLogger.info('Listening 0.0.0.0:8000');
 });
